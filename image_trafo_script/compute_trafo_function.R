9ded00a0ad461bf4c1f770797513ed9b9509a9d0
shiluus <dr.ulrich.wellner@gmail.com>
2016-03-11 15:54:00 +0100

    2016-03-11
    test on server

--- image_trafo_script/compute_trafo_function.R
+++ image_trafo_script/compute_trafo_function.R
@@ -1,61 +1,69 @@
 library(jpeg)
 library(EBImage)
 
-#load images-------------------------------------------------------------
+imgSrc<-NULL
+imgRef<-NULL
+refCoords<-NULL
+srcCoords<-NULL
+ZoomRef<-NULL
+ZoomSrc<-NULL
 
-msi <- readJPEG("msi.jpg")    #msi image
-he <- readJPEG("he.jpg")      #he image
+s<-NULL
+tr<-NULL
+alpha<-NULL
+n<-NULL
+input <- list()
 
-msi<-flip(msi)
-msi<-rotate(msi,angle = 90)
+hgt=300
 
-he<-flip(he)
-he<-rotate(he,angle = 90)
-
-display(msi)
-display(he)
-#!now chose ref coordinates from display in browser 
+imgRef <- readJPEG("MSI.jpg")    #MSI=ref image
+imgSrc <- readJPEG("HE.jpg")     #HE=src image
 
 
+p1<-c(17,96) 
+p2<-c(122,255) 
 
-#trafo-------------------------------------------------------------------
+q1<-c(16,19)
+q2<-c(122,178)
 
-p1<-c(126,34); p2<-c(240,123)  #two ref points(x,y) in msi image
-q1<-c(47,34); q2<-c(161,123)  #two ref points(x,y) in he image
+
+
+
+dot = sum((q2-q1)*(p2-p1))   # dot product
+d=det(matrix(c(q2-q1,p2-p1), ncol=2, byrow= TRUE)) # determinant
+alpha<- atan2(d, dot) 
+
+
+R<-matrix(c(cos(alpha), -sin(alpha), sin(alpha), cos(alpha)), nrow=2, byrow=TRUE) 
+
+
+tz<-c((dim(imgSrc)[1])/2,(dim(imgSrc)[2])/2)
+s<-abs((p2-p1)/(R%*%(q2-q1)))
+sm<-matrix(c(s[1], 0, 0, s[2]), ncol=2, byrow=TRUE)
 
 #rotation
-dot = sum((q2-q1)*(p2-p1))                         #dot product
-d=det(matrix(c(q2-q1,p2-p1), ncol=2, byrow= TRUE)) #determinant
-alpha<- atan2(d, dot)                              #rotation angle
-rot<-rotate(he, alpha*(180/pi))                      #rotate he image
-writeJPEG(rot,target = "rot.jpg")                    #save
+n<-rotate(imgSrc, alpha*(180/pi))
+writeJPEG(n,target = "rot.jpg")
 
-
-#generate resizing + translation parameters
-R<-matrix(c(cos(alpha), -sin(alpha),
-            sin(alpha), cos(alpha)),
-          nrow=2, byrow=TRUE)
-tz<-c((dim(he)[1])/2,(dim(he)[2])/2)
-s<-abs((p2-p1)/(R%*%(q2-q1)))           
-sm<-matrix(c(s[1], 0, 0, s[2]), ncol=2, byrow=TRUE)
-tzN<-c((dim(rot)[1])/2,(dim(rot)[2])/2)
+tzN<-c((dim(n)[1])/2,(dim(n)[2])/2)
 tr<-p1-sm%*%(R%*%(q1-tz)+tzN)
-wh=(dim(rot)[1:2])*s
+wh=(dim(n)[1:2])*s
 
-#resize+save
-rez<-resize(rot, wh[1], wh[2])
-writeJPEG(rez,target = "rez.jpg")
+#resize
+m<-resize(n, wh[1], wh[2])
+writeJPEG(m,target = "rez.jpg")
 
-#translate+save
-#he(tra) now has same coordinates as msi
-tra<-translate(rez, tr, output.dim=dim(msi)[1:2]) 
-writeJPEG(tra,target = "tra.jpg")
+#translate
+t<-translate(m, tr, output.dim=dim(imgRef)[1:2]) 
+writeJPEG(t,target = "tra.jpg")
+#src pic now has same coordinates as ref pic
 
-#generate overlay img for visual control
-ovl<-tra+msi-tra*msi
-writeJPEG(ovl, "ovl.jpg")
+#generate overlay
+n<-translate(m, tr, output.dim=dim(imgRef)[1:2]) 
+n<-n+imgRef-n*imgRef
+writeJPEG(n, "overlay.jpg")
 
-#get back-flipped/rotated img for further analysis
-tra<-rotate(tra,-90)
-tra<-flip(tra)
-writeJPEG(tra,target = "tra2.jpg")
+
+
+
+
