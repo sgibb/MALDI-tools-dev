9ed797a9d508d690bc84a7b47d57c722ed404209
shiluus <dr.ulrich.wellner@gmail.com>
2016-03-13 13:13:54 +0100

    2016-03-13
    trafo tool update

--- coordinate_trafo/trafo_use.R
+++ coordinate_trafo/trafo_use.R
@@ -1,4 +1,4 @@
-source("trafo.R")
+source("trafo_functions.R")
 
 he<-readImage("he.jpg")
 msi<-readImage("msi.jpg")
@@ -14,19 +14,18 @@ msi1<-c(126,34)
 msi2<-c(256,45)
 msi3<-c(240,124)
 
-#compute coordinate trafo matrices
-t <- trafo(he1,he2,he3,msi1,msi2,msi3) #he to msi matrix
-ti <- solve(t)                         #msi to he matrix
+#compute trafomatrix
+t <- get.trafomatrix(he1,he2,he3,msi1,msi2,msi3) #he to msi matrix
 
 #test coordinate conversion
-pointer.he <- c(47,35,1,1)
-pointer.msi <- t %*% pointer.he
-pointer.he2 <- ti %*% pointer.msi
+pointer.he <- c(47,35)
+pointer.msi <- get.msi(pointer.he,t)
+pointer.he2 <- get.he(pointer.msi,t)
 
 #test he image trafo to fit msi
-test <- heToMsi(he,msi,t)
+test <- map.he.on.msi(he,msi,t)
 display(test)
 
 #test msi image trafo to fit he
-test2 <- msiToHE(he,msi,t)
+test2 <- map.msi.on.he(he,msi,t)
 display(test2)
