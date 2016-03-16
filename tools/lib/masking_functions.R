869c79756000754d5647cdfda04e9edcce4335fd
shiluus <dr.ulrich.wellner@gmail.com>
2016-03-16 22:11:35 +0100

    prepro tool without IO

--- tools/lib/masking_functions.R
+++ tools/lib/masking_functions.R
@@ -6,17 +6,17 @@ get.mask.xy <- function (mask,value,label)
      mask <- as.array(mask)
      mask <- mask[,,1]
      xy <- which (mask==value, arr.ind = TRUE)
-     xyl <- as.data.frame(cbind(xy,label))
+     xy <- data.frame (xy)
+     xyl <- cbind(xy,label)
+     colnames(xyl) <- c("x","y","label")
      return(xyl)
 }
 
 
-get.msi.mask <- function (he.mask.file, trafo.matrix.file)
+get.msi.mask <- function (xyl.he, trafo.matrix.file)
 {
-     #read ROI x,y,label table and trafo matrix from csv files
-     t <- as.matrix(read.tab.uw(trafo.matrix.file))
-     xyl.he <- read.tab.uw(he.mask.file)
      xy.he <- as.matrix(xyl.he[,1:2])
+     t <- as.matrix(read.tab.uw(trafo.matrix.file))
      
      #get ROI msi coordinates from he coordinates
      xy.msi <- xy.he
@@ -24,7 +24,8 @@ get.msi.mask <- function (he.mask.file, trafo.matrix.file)
      {
           xy.msi[i,] <- round(get.msi(xy.he[i,],t))
      }
-     xyl.msi <- as.data.frame(cbind(xy.msi,as.character(xyl.he[,3])))
+     xy.msi <- data.frame (xy.msi)
+     xyl.msi <- cbind(xy.msi,as.character(xyl.he[,3]))
      colnames(xyl.msi) <- c("x","y","label")
      return(xyl.msi)
 }
--- tools/prepro.tool.R
+++ tools/prepro.tool.R
@@ -8,8 +8,9 @@ library (MALDIquantForeign)
 #library (MASS)
 
 #input var
-imzml <- "data/miniTMA.imzml"
+imzml             <- "data/miniTMA.imzml"
 tma.core.mask.jpg <- "data/tma.core.mask.jpg"
+trafo.matrix.file <- "data/trafomatrix_iso.txt"
 
 ##open pdf
 ##pdf('$outfile_plots')
@@ -20,54 +21,40 @@ tcm <- readImage(tma.core.mask.jpg)
 #extract xy from tma.core.mask
 value = 0
 label = "tma.core.mask"
-tcm.xyl <- get.mask.xy(tcm, value, label)
-write.tab.uw(tcm.xyl, file = paste("data/",label,".he.txt",sep = ""))
-
-#transform he mask to msi mask
-he.mask.file="data/tma.core.mask.he.txt"
-msi.mask.file="data/tma.core.mask.msi.txt"
-trafo.matrix.file="data/trafomatrix_iso.txt"
-xyl.msi <- get.msi.mask(he.mask.file,trafo.matrix.file)
-write.tab.uw(xyl.msi,msi.mask.file)
+xyl.he <- get.mask.xy(tcm, value, label)
+xyl.msi <- get.msi.mask(xyl.he,trafo.matrix.file)
 
 #import core areas from imzml
-msi.mask.file="data/tma.core.mask.msi.txt"
-tcm.xyl <- read.tab.uw(msi.mask.file)
-tcm.xy <- as.matrix(tcm.xyl[,1:2])
-
-print('Reading')
-#import imzml
+print('reading masked pixels from imzml')
+xy.msi <- as.matrix(xyl.msi[,1:2])
 s <- importImzMl(path = imzml,
-                 coordinates = tcm.xy)
+                 coordinates = xy.msi)
 
-msi <- get.msi.matrix(s,center = 1000, tolerance = 10)
-save.msi.jpeg(msi,file = "data/imported.msi.jpg")
+#check imported areas
+#msi <- get.msi.matrix(s,center = 1000, tolerance = 10)
+#save.msi.jpeg(msi,file = "data/imported.msi.jpg")
 
-print('transforming')
+print('transforming spectra')
 ##transformation
 st <- transformIntensity(s,
                          method="sqrt")
 
-
-print('smooting')
+print('smooting spectra')
 ##smoothing
 st <- smoothIntensity(st,
                       halfWindowSize=10)
 
-
-print('baseline removing')
+print('removing baseline from spectra')
 ##remove baseline
 st <- removeBaseline(st,
                      method="SNIP",
                      iterations=100)
 
-
-print('calibrate')
+print('calibrating spectra')
 ##calibrate
 st <- calibrateIntensity(st, method="TIC")
 
-
-print('align')
+print('aligning spectra')
 ##align spectra
 st <- alignSpectra(st, 
                    halfWindowSize=20,
@@ -75,30 +62,29 @@ st <- alignSpectra(st,
                    tolerance=0.002,
                    warpingMethod="lowess")
 
-
 ##average technical replicates: example
 ##samples <- factor(sapply(spectra, function(x)metaData(x)$sampleName))
 ##avgSpectra <- averageMassSpectra(spectra, labels=samples, method="mean")
 
+##save analysis results
+#save(st, file="st.R")
+exportImzMl(st, file="$outfile_imzml")
 
+
+
+#---------------------to be next tool---------------------------
 ##detect and bin peaks
-peaks <- detectPeaks(st,
-                     method="MAD",
-                     halfWindowSize=20,
-                     SNR=2)
-
-bins <- binPeaks(peaks, 
-                 tolerance=$binPeaks_tolerance)
-
+#peaks <- detectPeaks(st,
+#                     method="MAD",
+#                     halfWindowSize=20,
+#                     SNR=2)
+#bins <- binPeaks(peaks, 
+#                 tolerance=binPeaks_tolerance)
 #bins <- filterPeaks(bins, 
 #                    minFrequency=0.25)
+#save(peaks, file="peaks.R")
+#save(bins, file="bins.R")
 
-
-##save analysis results
-exportImzMl(st, file="$outfile_imzml")
-save(st, file="st.R")
-save(peaks, file="peaks.R")
-save(bins, file="bins.R")
 
 #closing plot
-dev.off()
+#dev.off()
